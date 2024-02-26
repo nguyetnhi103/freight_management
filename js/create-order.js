1c5b6e76b2e62ffe2bf7e80aafdbd018bd922a95
nguyetnhi103 <nguyetnhi10032003@gmail.com>
2024-02-27 01:14:28 +0700

    menu

---
+++ js/create-order.js
@@ -0,0 +1,16 @@
+function order_Menu() {
+    var menu = document.getElementsByClassName("menu")[0];
+    if (menu.style.display === "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+function staff_Menu() {
+    var staff = document.getElementsByClassName("staff")[0];
+    if (staff.style.display === "none") {
+        staff.style.display = "block";
+    } else {
+        staff.style.display = "none";
+    }
+}
